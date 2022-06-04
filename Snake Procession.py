6e88249df932ac228d3bc5440878c53e93706abc
Arnav Kumar <78733680+Arnavk194@users.noreply.github.com>
2022-06-04 15:08:33 +0530

    Update Snake Procession.py

--- Snake Procession.py
+++ Snake Procession.py
@@ -1,17 +1,21 @@
+result = []
 def ex():
     n = int(input())
     a = input()
     count = 0
-    if(a[i] == 'H'):
-        count +=1
-    elif(a[i] == 'T'):
-        count -=1
-    elif(count < 0 or count < 1):
-        print("Invalid")
-        return          
-
-
-
-
-t = int(input())
-for i in range(t):
+    for i in range(n):
+        if(a[i] == 'H'):
+            count +=1
+        elif(a[i] == 'T'):
+            count -=1
+        if(count < 0 or count > 1):
+            result.append("Invalid")
+            return          
+    if(count==0):
+        result.append("Valid")
+    else:
+        result.append("Invalid")
+for t in range(int(input())):
+    ex()
+for p in range(len(result)):
+    print(result[p])
